20dbebc17f0895804b7075ebad59ecbd7980c717
okowa42 <a.hirohara.0526@gmail.com>
2026-06-10 16:12:20 +0900

    Add robots.txt, fix vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+﻿import { defineConfig } from 'vite'
+import react from 'itejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
 })
